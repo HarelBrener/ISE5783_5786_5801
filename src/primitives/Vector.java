98e0ab3cbd423c3c61fc8884524fd0c971fef1d1
Harel Brener <hcbrener@gmail.com>
2023-03-02 17:59:54 +0200

    new proj

--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -6,10 +6,18 @@ public class Vector extends Point {
 
     Vector(Double3 p) {
         super(p);
+        if (zero.equals(p)){
+            throw new IllegalArgumentException("The vector is the zero vector");
+        }
     }
 
     public Vector(double a, double b, double c) {
         super(a,b,c);
+        if (zero.equals(p)){
+            throw new IllegalArgumentException("The vector is the zero vector");
+        }
     }
+
+
 
 }
